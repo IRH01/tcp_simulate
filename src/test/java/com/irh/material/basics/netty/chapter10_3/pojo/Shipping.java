0d8ba8a6faa3f50c7066a403b72cfb1af12748f9
iritchie.ren <Renh1298@13322.com>
2017-11-30 15:36:31 +0800

    测试 æ提价

--- src/test/java/com/irh/material/basics/netty/chapter10_3/pojo/Shipping.java
+++
@@ -1,8 +0,0 @@
-package com.irh.material.basics.netty.chapter10_3.pojo;
-
-/**
- * Created by Iritchie.ren on 2017/3/6.
- */
-public enum Shipping{
-    STANDARD_MALL, PRIORITY_MALL, INTERNATIONAL_MALL, DOMESTIC_EXPRESS, INTERNATIONAL_EXPRESS
-}
--- src/test/java/com/irh/material/basics/netty/chapter10_3/shell/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-java -jar  jibxtools.jar -f bind.xml Customer
-
-java -jar jibx-bind.jar binding.xml
-
-java -cp bin;E:\jar\lib\jibx-tools.jar org.jibx.binding.generator.BindGen -b binding.xml nettyinprotocol.xml.pojo.Order
